a73f5c42909624e93dd632a696199e5cfbf770f4
MD RUHUL AMIN <mdruhulamin@email.com>
2025-12-06 17:18:29 +0700

    query optimised

--- phimart/settings.py
+++ phimart/settings.py
@@ -91,14 +91,15 @@ INTERNAL_IPS = [
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': config('db_name'),
-       'USER': config('db_user'),
-       'PASSWORD': config('db_password'),
-       'HOST': config('db_host'),
-       'PORT': config('db_port'),
+       'NAME': config('DB_NAME'),
+       'USER': config('DB_USER'),
+       'PASSWORD': config('DB_PASSWORD'),
+       'HOST': config('DB_HOST'),
+       'PORT': config('DB_PORT'),
    }
 }
 # Password validation
@@ -123,9 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Configuration       
 
 cloudinary.config(
-    cloud_name=config('cloudinary_cloud_name'),
-    api_key=config('cloudinary_api_key'),
-    api_secret=config('cloudinary_api_secret'),
+    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+    api_key=config('CLOUDINARY_API_KEY'),
+    api_secret=config('CLOUDINARY_API_SECRET'),
     secure=True
 )
 # Cloudenery media stroge setting
--- product/views.py
+++ product/views.py
@@ -30,7 +30,7 @@ class ProductViewSet(ModelViewSet):
      - support serach by name ,description and category
      - support ordering by price and updated_at
     """
-    queryset=Product.objects.all()
+    
     serializer_class=ProductSerializer
     permission_classes=[AllowAny]
 
@@ -39,7 +39,8 @@ class ProductViewSet(ModelViewSet):
     search_fields=['name','description','category__name']
     ordering_fields=['price','updated_at']
     pagination_class=DefaultPagination
-
+    def get_queryset(self):
+        return Product.objects.prefetch_related('images').all()
     @swagger_auto_schema(
         operation_summary='Retrive all list of product',
        
