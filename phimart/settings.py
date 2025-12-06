8a4db4ac88f7aa18ddb07a0ea7f1ed2da873b53e
MD RUHUL AMIN <mdruhulamin@email.com>
2025-12-06 17:25:37 +0700

    query optimised

--- phimart/settings.py
+++ phimart/settings.py
@@ -95,11 +95,11 @@ INTERNAL_IPS = [
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': config('DB_NAME'),
-       'USER': config('DB_USER'),
-       'PASSWORD': config('DB_PASSWORD'),
-       'HOST': config('DB_HOST'),
-       'PORT': config('DB_PORT'),
+       'NAME': config('db_name'),
+       'USER': config('db_user'),
+       'PASSWORD': config('db_password'),
+       'HOST': config('db_host'),
+       'PORT': config('db_port'),
    }
 }
 # Password validation
@@ -122,13 +122,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # configration for cloudness storages
 # Configuration       
-
 cloudinary.config(
-    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
-    api_key=config('CLOUDINARY_API_KEY'),
-    api_secret=config('CLOUDINARY_API_SECRET'),
+    cloud_name=config('cloudinary_cloud_name'),
+    api_key=config('cloudinary_api_key'),
+    api_secret=config('cloudinary_api_secret'),
     secure=True
 )
+
 # Cloudenery media stroge setting
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Internationalization
